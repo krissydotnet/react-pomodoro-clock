d3dd7a538ccecce85212b0e4a5b7a87b08824ff2
Krissy Dukes <krissydotnet@yahoo.com>
2019-03-29 08:45:04 -0400

    Updates

---
+++ precache-manifest.b33c45ec2ca3001a2fc47853010ab5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "988833bf34d92eba3d536029cd9e1bc6",
+    "url": "/react-pomodoro-clock/static/media/BeepSound.988833bf.wav"
+  },
+  {
+    "revision": "6719385d069ef5bb6fdf",
+    "url": "/react-pomodoro-clock/static/js/runtime~main.8f834abd.js"
+  },
+  {
+    "revision": "fd745e4d0583721bcfdf",
+    "url": "/react-pomodoro-clock/static/js/main.e0d4ef88.chunk.js"
+  },
+  {
+    "revision": "b7dd11a246c46891a255",
+    "url": "/react-pomodoro-clock/static/js/2.fee03bd9.chunk.js"
+  },
+  {
+    "revision": "fd745e4d0583721bcfdf",
+    "url": "/react-pomodoro-clock/static/css/main.a6e60e12.chunk.css"
+  },
+  {
+    "revision": "a0f42f394b5fa29f6e3919f1ea38d47d",
+    "url": "/react-pomodoro-clock/index.html"
+  }
+];
